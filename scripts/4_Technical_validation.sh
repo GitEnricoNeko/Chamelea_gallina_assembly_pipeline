0c8dac4944d59fb75afeabc43f7e1ba73c0cf463
Enrico <75318813+GitEnricoNeko@users.noreply.github.com>
2025-03-24 16:49:03 +0100

    Update and rename Technical_validation.sh to 4_Technical_validation.sh

--- scripts/4_Technical_validation.sh
+++ scripts/4_Technical_validation.sh
@@ -14,3 +14,12 @@ busco -i $Genome_prefix.hic.a.fasta -o busco_camelea -l metazoa -c 8 -m geno -f
 busco -i $Genome_prefix.hic.a.fasta -o busco_camelea -l mollusca -c 8 -m geno -f 
 ```
 
+## merqury
+
+```bash
+meryl k=21 count output reads1.meryl DNA_short_1.fq
+meryl k=21 count output reads2.meryl DNA_short_2.fq
+meryl union-sum output genome.meryl read*.meryl
+## run merqury
+merqury.sh genome.meryl primary.fasta alternate.fasta final
+```
